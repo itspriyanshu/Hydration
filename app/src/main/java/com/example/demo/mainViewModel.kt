061cc2efa8230bb53e7f1af797363634c36bb794
Priyanshu <askpriyanshu30@gmail.com>
2020-10-16 15:22:00 +0530

    Retrofit Added

--- app/src/main/java/com/example/demo/mainViewModel.kt
+++ app/src/main/java/com/example/demo/mainViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.demo
 
+import android.net.DnsResolver
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.loader.content.AsyncTaskLoader
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import retrofit2.Call
+import retrofit2.Response
 import java.util.*
+import javax.security.auth.callback.Callback
 import kotlin.collections.ArrayList
 
 class mainViewModel : ViewModel() {
@@ -16,9 +22,20 @@ class mainViewModel : ViewModel() {
         water_count.value = 0
     }
 
+    // Working Retrofit
     fun addonecount(){
         Log.i("CustomTAG","addtocount called"+water_count.value)
         water_count.value = water_count.value?.plus(1)
         time_list.add(Date().toString())
+        service.query.getall().enqueue(object : retrofit2.Callback<JsonArray>{
+            override fun onFailure(call: Call<JsonArray>, t: Throwable) {
+                Log.i("Google", t.toString())
+            }
+
+            override fun onResponse(call: Call<JsonArray>, response: Response<JsonArray>) {
+                Log.i("Google", response.body().toString())
+            }
+
+        })
     }
 }
